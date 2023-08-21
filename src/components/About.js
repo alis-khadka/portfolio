7d6bf7035f837111e0873cf4d3626ca4f2e476a7
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-08-21 20:57:46 +0545

    feat(): About page changes

--- src/components/About.js
+++ src/components/About.js
@@ -9,28 +9,30 @@ function About() {
 				Back in 2014, I decided to try my hand at creating simple calculator app 
 				for my android smartphone which got me into coding and web development.
 			</p>
-			<p className='mb-4'>
+
+			<p className='mb-2'>
 				Fast-forward to today, with over 3.5 years of professional experience in software 
 				engineering, I’ve had the privilege of building softwares in domains 
 				like:
-				<ul className='mt-1 px-3'>
-					<li>
-						<a className='paragraph-highlights hover-underline-animation d-inline' href='https://oho.bajratechnologies.com/service/mall' target='_blank' rel='noreferrer'>
-							E-commerce
-						</a>
-					</li>
-					<li>
-						<a className='paragraph-highlights hover-underline-animation d-inline' href='https://www.copper.com/' target='_blank' rel='noreferrer'>
-							Customer Relationship Management (CRM)
-						</a>
-					</li>
-					<li>
-						<a className='paragraph-highlights hover-underline-animation d-inline' href='https://trip101.com/' target='_blank' rel='noreferrer'>
-							Travel & Tourism Centric Blogging Platform
-						</a>
-					</li>
-				</ul>
 			</p>
+
+			<ul className='mb-4 px-3'>
+				<li>
+					<a className='paragraph-highlights hover-underline-animation d-inline' href='https://oho.bajratechnologies.com/service/mall' target='_blank' rel='noreferrer'>
+						E-commerce
+					</a>
+				</li>
+				<li>
+					<a className='paragraph-highlights hover-underline-animation d-inline' href='https://www.copper.com/' target='_blank' rel='noreferrer'>
+						Customer Relationship Management (CRM)
+					</a>
+				</li>
+				<li>
+					<a className='paragraph-highlights hover-underline-animation d-inline' href='https://trip101.com/' target='_blank' rel='noreferrer'>
+						Travel & Tourism Centric Blogging Platform
+					</a>
+				</li>
+			</ul>
 
 			<p>
 				My main focus these days has been in building and contributing 
