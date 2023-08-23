e8271bb7e444083810dae97efb948fbc9426da5b
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-08-23 08:16:56 +0545

    feat(): add custom icons

--- src/custom_icons/index.js
+++ src/custom_icons/index.js
@@ -1,2 +1,8 @@
+export { faAntdesignIcon } from "./fa-antdesign-icon";
+export { faJqueryIcon } from "./fa-jquery-icon";
+export { faMysqlIcon } from "./fa-mysql-icon";
+export { faNextjsIcon } from "./fa-nextjs-icon";
+export { faPostgresqlIcon } from "./fa-postgresql-icon";
 export { faRubyIcon } from "./fa-ruby-icon";
 export { faRubyOnRailsIcon } from "./fa-ruby-on-rails-icon";
+export { faSqlIcon } from "./fa-sql-icon";
