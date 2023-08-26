8fa80ba0ec3c462bdeffb6e9a1df0dfaf045f9cd
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-08-26 13:59:38 +0545

    feat(): conditionally render scrollable content

--- src/components/PortfolioContent.js
+++ src/components/PortfolioContent.js
@@ -1,12 +1,36 @@
 import './PortfolioContent.scss';
 import FixedContent from './FixedContent';
 import ScrollableContent from './ScrollableContent';
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
+import Skills from './Skills';
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 function PortfolioContent(props) {
+	const isLargeScreen = useMediaQuery("(min-width : 992px)");
+
 	return (
 		<div className='portfolio-content'>
 			<FixedContent contactRef={props.contactRef} />
-			<ScrollableContent {...props} />
+
+			{ isLargeScreen ? (
+				<ScrollableContent {...props} />
+			) : (
+				<>
+					<span ref={props.aboutRef} className='h-0 opacity-0'/>
+					<About />
+
+					<span ref={props.skillsRef} className='h-0 opacity-0'/>
+					<Skills />
+
+					<span ref={props.projectsRef} className='h-0 opacity-0'/>
+					<Projects />
+
+					<span ref={props.contactRef} className='h-0 opacity-0'/>
+					<Contact />
+				</>
+			)}
 		</div>
 	);
 }
