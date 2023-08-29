1dd0f31498b128615ba77be4a5b2388bd59b2258
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-08-29 21:56:35 +0545

    feat(): changes to incorporate html strings in projects section

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -11,7 +11,7 @@ function ProjectItem(props) {
                 <img className="card-img-top" src={project.image} alt="Card cap" />
                 <div className="card-body d-flex flex-column pt-0">
                     <h5 className='card-title fw-bold'><span className=' hover-underline-animation'>{project.name}</span></h5>
-                    <p className='card-text text-left mb-1'>{project.description}</p>
+                    <p className='card-text text-left mb-1' dangerouslySetInnerHTML={{__html: project.description}}></p>
                     <p className='card-text text-left mb-0'><span className='hover-underline-animation'>Responsibilities:</span></p>
                     <ul>
                         {project.responsibilities.map((responsibility, index) => {
@@ -28,7 +28,7 @@ function ProjectItem(props) {
                             project.githubLinks &&
                             (
                                 project.githubLinks.map((item, index) => {
-                                    return <a className='hover-underline-animation' href={item.link} target='_blank' rel='noreferrer'>{item.label || 'View Code'}&nbsp;<FontAwesomeIcon icon={faGithub} /></a>
+                                    return <a key={index} className='hover-underline-animation' href={item.link} target='_blank' rel='noreferrer'>{item.label || 'View Code'}&nbsp;<FontAwesomeIcon icon={faGithub} /></a>
                                 }) 
                             )
                         }
@@ -36,7 +36,7 @@ function ProjectItem(props) {
                             project.liveLinks &&
                             (
                                 project.liveLinks.map((item, index) => {
-                                    return <a className='hover-underline-animation' href={item.link} target='_blank' rel='noreferrer'>{item.label || 'View Code'}&nbsp;<FontAwesomeIcon icon={faLink} /></a>
+                                    return <a key={index} className='hover-underline-animation' href={item.link} target='_blank' rel='noreferrer'>{item.label || 'View Live'}&nbsp;<FontAwesomeIcon icon={faLink} /></a>
                                 }) 
                             )
                         }
