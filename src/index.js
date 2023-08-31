6a03cd73b377483ce74764637fecdfe33e45ed9a
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-08-31 21:07:43 +0545

    feat(): add vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
