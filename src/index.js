3a6f746f0ab8684c0630c9a642de8d7dc2322a39
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-09-14 15:26:01 +0545

    feat(): implement react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.scss';
 import App from './App';
+import Resume from './Resume';
 import reportWebVitals from './reportWebVitals';
 import { Analytics } from '@vercel/analytics/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/resume",
+    element: <Resume />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <Analytics />
   </React.StrictMode>
 );
