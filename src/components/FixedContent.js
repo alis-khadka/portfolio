259def920a71bc11214509cd8550ae8eeaf2b723
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2024-09-01 16:46:32 -0500

    chore(): some css for the announcement section

--- src/components/FixedContent.js
+++ src/components/FixedContent.js
@@ -13,7 +13,7 @@ function FixedContent(props) {
 		<div className='fixed-container d-flex flex-column justify-content-center px-5'>
 			<img src={headshot} alt='Prashant pic' className='self-image' />
 			<Notice />
-			<h1 className='mb-5'>Hi. I'm a Software Engineer.</h1>
+			<h1 className='mb-3 mb-sm-5'>Hi. I'm a Software Engineer.</h1>
 			<div className='introduction-text'>My name's Prashant Khadka and I craft robust and scalable web applications using modern web technologies.</div>
 			<button type='button' className='contact-button btn mt-5 d-lg-none' onClick={handleClick}>Let's work together</button>
 			<div className='links-container'>
